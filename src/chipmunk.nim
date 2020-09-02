d050b3a68de608edfea6ef99bd08bdb8e1111834
Quentin Barbarat <quentin.barbarat@epita.fr>
2020-09-02 15:51:20 +0200

    logging: add logging to rom loading

--- src/chipmunk.nim
+++ src/chipmunk.nim
@@ -1,34 +1,43 @@
-import streams
-import os
+import os, streams, logging, strformat
 
 type
     CPU* = object
         ## Type for the Chip8 CPU
         v*: array[16, uint8]
-        ## The V registers
+            ## The V registers
         i*: uint16
-        ## The I register
+            ## The I register
         pc*: uint16
-        ## The program counter
+            ## The program counter
         sp*: uint8
-        ## The stack pointer
+            ## The stack pointer
         dt*: uint8
-        ## The delay timer
+            ## The delay timer
         st*: uint8
-        ## The sound timer
+            ## The sound timer
 
-type
     Chip8* = object
         ## Type for the CHIP-8 itself
         memory*: array[4096, uint8]
-        ## The RAM
+            ## The RAM
+        CPU*: CPU
+            ## The CPU
 
 proc load_rom*(c: var Chip8, path: string) =
+    ## Loads the rom at the specified path into CHIP-8 RAM
+
+    info(fmt"Trying to load: {path}")
+
     let romSize = getFileSize(path)
     doAssert int(romSize) < (4096 - 0x200)
 
     let rom = newFileStream(path, fmRead)
     discard rom.readData(addr(c.memory[0x200]), int(romSize))
 
+var logger = newConsoleLogger(fmtStr="[$time] - $levelname: ")
+addHandler(logger)
+
 var plop: Chip8
-plop.load_rom("INVADERS")
+
+let path = (if paramCount() > 0: paramStr(1) else: "./roms/INVADERS")
+plop.load_rom(path)
